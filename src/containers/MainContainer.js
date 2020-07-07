2c81a1844e8f96e2bfe1adf2b485dd4c09e65cea
mark <markpackham1@gmail.com>
2020-07-07 12:41:26 +0100

    Initial commit

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class MainContainer extends Component{
+    render(){
+        return(
+            <h1>React is working</h1>
+        )
+    }
+}
