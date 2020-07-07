22a4589841ef2dc19d1f4298d59e1043481b472b
mark <markpackham1@gmail.com>
2020-07-07 16:00:09 +0100

    add image alt and test get Year

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -15,6 +15,8 @@ export default class MainContainer extends Component{
             selectedActorName: ""
         };
 
+        let theYear = new Date().getFullYear();
+        console.log(theYear);
         this.handleCharacterSelected = this.handleCharacterSelected.bind(this);
     }
 
