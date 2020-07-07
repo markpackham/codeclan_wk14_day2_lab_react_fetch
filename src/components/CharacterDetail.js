8147071f5fbb09b8c08abc014f53038383214b4c
mark <markpackham1@gmail.com>
2020-07-07 14:45:11 +0100

    Reduce amount of curley brackets used

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const CharacterDetail = ({selectedCharacter}) => {
-    if (!{selectedCharacter}) return null;
+    if (!selectedCharacter) return null;
 
     return (
         <div className="character-detail">
-        <h3>{{selectedCharacter}.name}</h3>
-        <h4>Species: {{selectedCharacter}.species}</h4>
-        <h4>House: {{selectedCharacter}.house}</h4>
+        <h3>{selectedCharacter.name}</h3>
+        <h4>Species: {selectedCharacter.species}</h4>
+        <h4>House: {selectedCharacter.house}</h4>
         </div>
     );
 }
