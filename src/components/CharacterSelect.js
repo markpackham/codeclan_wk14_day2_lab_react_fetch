8339c6cc2cfc9b57611cf950abf6ea5fb3333d11
mark <markpackham1@gmail.com>
2020-07-07 14:02:38 +0100

    Set things up for character selection

---
+++ src/components/CharacterSelect.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CharacterSelect = (props) => {
+
+    const optionsList = props.characters.map(character =>{
+    return <option value={character.name} key={character.name}>{character.name}</option>
+    });
+
+    function handleChange(event){
+        props.onCharacterSelected(event.target.value);
+    }
+
+    return (
+        <select id="character-selector" defaultValue="default" onChange={handleChange}>
+            <option disabled value="default">
+                Please select a character...
+            </option>
+            {optionsList}
+        </select>
+    )
+}
+
+export default CharacterSelect;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import CharacterSelect from "../components/CharacterSelect";
 import MainFooter from '../components/MainFooter';
 
 export default class MainContainer extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            characters: [],
+            // all characters names are unique so should be keys
+            selectedCharacterName: ""
+        };
+
+        this.handleNameSelected = this.handleNameSelected.bind(this);
+    }
+
+// make sure we can actually access the Harry Potter api
+componentDidMount(){
+    const url = 'http://hp-api.herokuapp.com/api/characters';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => this.setState({characters: data}))
+}
+
+handleNameSelected(name){
+    this.setState({selectedCharacterName: name})
+}
+
+
     render(){
+
+const selectedName = this.state.characters.find(character => {
+    return character.name = this.state.selectedCharacterName;
+})
+
         return(
-            <MainFooter />
+            <div className="content">
+                <h2>Harry Potter characters</h2>
+                    <CharacterSelect 
+                    characters={this.state.characters}
+                    onCharacterSelected = {this.handleNameSelected}
+                    />
+                    <MainFooter />
+            </div>
+           
         )
     }
 }
